fb157d949607310fa5f48bb550e423f13c6c99c0
Matthieu <matthieu.desmarescaux@isen.yncrea.fr>
2018-03-30 17:14:11 +0200

    Auto stash before merge of "master" and "origin/master"

--- Projet vierge/js/controller/GameUpdate.js
+++ Projet vierge/js/controller/GameUpdate.js
@@ -6,25 +6,25 @@ function(){
     var xm = 0;
     var ym = 0;
   if(this.input.keyboard.isDown(Phaser.Keyboard.LEFT)){//On regarde si on appuiye sur la fleche de gauche
-    if(carte[Math.round(player.x/65 -1)][Math.round(player.y/65)] != 1){//On verifie si le joeur est toujour dans la casse
+    if(carte[Math.floor(player.x/65 -1)][Math.floor(player.y/65)] != 1){//On verifie si le joeur est toujour dans la casse
       x -= 1;
       player.play('left');
     }
   }
   else if(this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){//...fleche droit
-    if(carte[Math.round(player.x/65 + 1)][Math.round(player.y/65)] != 1){
+    if(carte[Math.floor(player.x/65 + 1)][Math.floor(player.y/65)] != 1){
       x += 1;
       player.play('right');
     }
   }
   else if(this.input.keyboard.isDown(Phaser.Keyboard.UP)){//...fleche Haut
-    if(carte[Math.round(player.x/65)][Math.round(player.y/65 - 1)] != 1){
+    if(carte[Math.floor(player.x/65)][Math.floor(player.y/65 - 1)] != 1){
       y -= 1;
       player.play('up');
     }
   }
   else if(this.input.keyboard.isDown(Phaser.Keyboard.DOWN)){//...fleche bas
-    if(carte[Math.round(player.x/65)][Math.round(player.y/65 + 1)] != 1){
+    if(carte[Math.floor(player.x/65)][Math.floor(player.y/65 + 1)] != 1){
       y += 1;
       player.play('down');
     }
@@ -37,8 +37,8 @@ function(){
   player.y += y //+= x/2 + y/2;
   afficher.x += x//x - y;
   afficher.y += y//(x + y)/2;
-  console.log(Math.round(player.x/65));
-  console.log(Math.round(player.y/65));
+  console.log(Math.floor(player.x/65));
+  console.log(Math.floor(player.y/65));
 }
   return update;
 });
