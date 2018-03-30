2b740556f0abe03b9238a015c195b01e630f9317
Matthieu <matthieu.desmarescaux@isen.yncrea.fr>
2018-03-30 17:09:49 +0200

    map lol

--- Projet vierge/js/controller/GameCreate.js
+++ Projet vierge/js/controller/GameCreate.js
@@ -5,6 +5,27 @@ function(){
     //img1 = this.add.sprite(-1280+500,-960+225,'background');
     //img1.scale.setTo(2,2);
 
+    // === création de la map ===
+    carte = new Array();
+    for(var i=0; i < 10; i++)
+      carte[i] = new Array();
+    for(let i = 0; i < 10; i++){
+      for(let y = 0; y < 10; y++){
+        carte[i][y] = 0;
+        if(i == 0 || i == 10-1 || y == 0 || y == 10-1 )
+          carte[i][y] = 1;
+      }
+    }
+
+    //==== initialisation des pixel ===>> (représentation du tableau)
+    for(let i in carte)
+      for(let y in carte[i])
+        if(carte[i][y] != 1)
+          GreyPixel = this.add.sprite(65*i,65*y,"GreyPixel");
+        else {
+          RedPixel = this.add.sprite(65*i,65*y,"RedPixel");
+        }
+
     // === initialisation du pseudo text ===
     afficher = this.add.text(200+5,200-5,input.value,{font: "16px Arial"});
 
@@ -17,25 +38,6 @@ function(){
     player.animations.add('left', [9,10,11,12,13,14,15,16,17], 10, true); // .... du move gauche
     player.animations.add('down', [18,19,20,21,22,23,24,25,26], 10, true);// .... du move arrière
     player.animations.add('right', [27,28,29,30,31,32,33,34,35], 10, true); // ... du move droite
-
-    // === création de la map ===
-    carte = new Array();
-    for(var i=0; i < 40; i++)
-      carte[i] = new Array();
-    for(let i = 0; i < 40; i++){
-      for(let y = 0; y < 40; y++){
-        carte[i][y] = 0;
-        if(i == 0 || i == 40-1 || y == 0 || y == 40-1 )
-          carte[i][y] = 1;
-      }
-    }
-
-    //==== initialisation des pixel ===>> (représentation du tableau)
-    for(let i in carte)
-      for(let y in carte[i])
-        if(carte[i][y] != 1)
-          pixel10x10 = this.add.sprite(65*i,65*y,"pixel10x10")
-
 	}
   return create;
 });
--- Projet vierge/js/controller/GamePreload.js
+++ Projet vierge/js/controller/GamePreload.js
@@ -3,11 +3,12 @@ function(){
   var preload = function(){
       // === Chargement ===
       this.load.image('background', 'img/citybackground.jpg');
-      this.load.image('pixel10x10', 'img/dixPixels.png');
-
+      this.load.image('GreyPixel', 'img/GreyPixel.bmp');
+      this.load.image('RedPixel', 'img/RedPixel.bmp');
       // === Variable ===
       var s; // variable du Personnage qui bouge.
-      var pixel10x10;//variable du pixel 10x10
+      var GreyPixel;
+      var RedPixel;
       //var img1; // variable du fond'écran.
       var player; // variable du joeur
       var carte; // tableau de la map
