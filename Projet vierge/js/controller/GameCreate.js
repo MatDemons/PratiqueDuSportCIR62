6045126c10896063624335446b2069ac9a15f6cd
Matthieu <matthieu.desmarescaux@isen.yncrea.fr>
2018-04-13 16:41:57 +0200

    correction des bug + ajout de la caméra

--- Projet vierge/js/controller/GameCreate.js
+++ Projet vierge/js/controller/GameCreate.js
@@ -6,7 +6,6 @@ function(){
     // === initialisation du personnage qui bouge ===
     player = this.add.sprite(64,65, etat, 1); // je crée le player en fonction du Personnage
     this.physics.enable(player, Phaser.Physics.ARCADE);
-    player.body.collideWorldBounds = true;
     // === initialisation d'une valeur d'objet
     player.valeur = 0;
 
@@ -17,21 +16,24 @@ function(){
     // === initialisation de la voiture ===
     voiture = this.add.sprite(200,100,'voiture',1);
     this.physics.enable(voiture, Phaser.Physics.ARCADE);
-    voiture.body.collideWorldBounds = true;
 
     //==== initialisation des pixel ===>> (représentation du tableau)
-    for(let i = 0; i < 10; ++i)
-      for(let y = 0; y < 5; ++y)
-        if(!(i == 0 || y == 0 || i == 9 || y == 4))
+    for(let i = 0; i <= 10; ++i)
+      for(let y = 0; y <= 10; ++y)
+        if(!(i == 0 || y == 0 || i == 10 || y == 10))
           GreyPixel = this.add.sprite(64*i,65*y,"GreyPixel");
         else
         {
           RedPixel = this.add.sprite(64*i,65*y,"RedPixel")
           this.physics.enable(RedPixel, Phaser.Physics.ARCADE);
-          RedPixel.body.collideWorldBounds = true;
           RedPixel.body.immovable = true;
           Group.add(RedPixel);
         }
+    voiture.animations.add('up', [2], 0, true);   // je selection les different image du move avant
+    voiture.animations.add('left', [4], 0, true); // .... du move gauche
+    voiture.animations.add('down', [6], 0, true); // .... du move arrière
+    voiture.animations.add('right', [7], 0, true);// ... du move droite
+    voiture.bringToTop();
 
     // === initialisation les different déplacement du personnage ===
     player.animations.add('up', [0,1,2,3,4,5,6,7,8], 10, true); // je selection les different image du move avant
@@ -40,11 +42,11 @@ function(){
     player.animations.add('right', [27,28,29,30,31,32,33,34,35], 10, true); // ... du move droite
     player.bringToTop()
 
-    voiture.animations.add('up', [2], 0, true);   // je selection les different image du move avant
-    voiture.animations.add('left', [4], 0, true); // .... du move gauche
-    voiture.animations.add('down', [6], 0, true); // .... du move arrière
-    voiture.animations.add('right', [7], 0, true);// ... du move droite
-    voiture.bringToTop();
+    // === initialisation de la caméra
+    this.world.setBounds(0, 0, 64*11, 65*11);
+    this.physics.startSystem(Phaser.Physics.P2JS);
+    player.body.fixedRotation = true;
+    voiture.body.fixedRotation = true;
 	}
   return create;
 });
--- Projet vierge/js/controller/GameUpdate.js
+++ Projet vierge/js/controller/GameUpdate.js
@@ -5,22 +5,31 @@ function(){
     voiture.body.velocity.set(0);
     this.physics.arcade.collide(player, Group);
     this.physics.arcade.collide(voiture,  Group);
-    //this.physics.arcade.collide(player, voiture);
   if(this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
     switch (player.valeur) {
       case 0:
       {
-        if(this.physics.arcade.collide(player, voiture)){
-          player.valeur = 1;
-          player.visible = false;
+        if (this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).downDuration(1))
+        {
+          if(this.physics.arcade.collide(player, voiture)){
+            player.valeur = 1;
+            player.visible = false;
+            this.camera.follow(voiture, Phaser.Camera.FOLLOW_LOCKON, 0.5, 0.5);
+          }
+          break;
         }
-        break;
       }
       case 1:
       {
-        player.valeur = 0;
-        player.visible = true;
-        break;
+        if (this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).downDuration(1))
+        {
+          this.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.5, 0.5);
+          player.valeur = 0;
+          player.x = voiture.x+60;
+          player.y = voiture.y+20;
+          player.visible = true;
+          break;
+        }
       }
     }
   }
@@ -28,46 +37,46 @@ function(){
   if(this.input.keyboard.isDown(Phaser.Keyboard.LEFT)){//On regarde si on appuiye sur la fleche de gauche
       switch (player.valeur) {
         case 0: {player.play('left');
+                player.body.velocity.x = -100;
                 break;}
         case 1:{ voiture.play('left');
-            voiture.body.velocity.x = -100;
+            voiture.body.velocity.x = -200;
                 break;}
       }
-      player.body.velocity.x = -100;
   }
   else if(this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){//...fleche droit
       switch (player.valeur) {
         case 0: {player.play('right');
+              player.body.velocity.x = 100;
                 break;}
         case 1: {voiture.play('right');
-              voiture.body.velocity.x = 100;
+              voiture.body.velocity.x = 200;
                 break;}
       }
-      player.body.velocity.x = 100;
   }
   else if(this.input.keyboard.isDown(Phaser.Keyboard.UP)){//...fleche Haut
       switch (player.valeur) {
         case 0: {player.play('up');
+            player.body.velocity.y = -100;
                 break;}
         case 1: {voiture.play('up');
-            voiture.body.velocity.y = -100;
+            voiture.body.velocity.y = -200;
                 break;}
       }
-      player.body.velocity.y = -100;
   }
   else if(this.input.keyboard.isDown(Phaser.Keyboard.DOWN)){//...fleche bas
       switch (player.valeur) {
         case 0: {
+          player.body.velocity.y = 100;
           player.play('down');
           break;
         }
         case 1:{
           voiture.play('down');
-          voiture.body.velocity.y = 100;
+          voiture.body.velocity.y = 200;
           break;
         }
       }
-          player.body.velocity.y = 100;
 
   }
   else
