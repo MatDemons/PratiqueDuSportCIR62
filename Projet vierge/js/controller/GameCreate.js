e253f47d72ca469f527ba054ce09499cd25e6828
Matthieu <matthieu.desmarescaux@isen.yncrea.fr>
2018-04-20 15:49:27 +0200

    velo + bus

--- Projet vierge/js/controller/GameCreate.js
+++ Projet vierge/js/controller/GameCreate.js
@@ -17,6 +17,15 @@ function(){
     voiture = this.add.sprite(200,100,'voiture',1);
     this.physics.enable(voiture, Phaser.Physics.ARCADE);
 
+    // === initialisation de la Bus ===
+    bus = this.add.sprite(500,200,'bus',1);
+    this.physics.enable(bus, Phaser.Physics.ARCADE);
+
+    // === initialisation de la velo ===
+    velo = this.add.sprite(10,10,'velo',1);
+    this.physics.enable(velo, Phaser.Physics.ARCADE);
+    velo.scale.setTo(0.25,0.25);
+
     //==== initialisation des pixel ===>> (représentation du tableau)
     for(let i = 0; i <= 100; ++i)
       for(let y = 0; y <= 100; ++y)
@@ -27,11 +36,22 @@ function(){
           RedPixel.body.immovable = true;
           Group.add(RedPixel);
         }
+    // === initialisation les different déplacement du voiture ===
     voiture.animations.add('up', [2], 0, true);   // je selection les different image du move avant
     voiture.animations.add('left', [4], 0, true); // .... du move gauche
     voiture.animations.add('down', [6], 0, true); // .... du move arrière
     voiture.animations.add('right', [7], 0, true);// ... du move droite
     voiture.bringToTop();
+
+    // === initialisation les different déplacement du Bus ===
+    bus.animations.add('up', [0], 0, true);   // je selection les different image du move avant
+    bus.animations.add('left', [2], 0, true); // .... du move gauche
+    bus.animations.add('down', [3], 0, true); // .... du move arrière
+    bus.animations.add('right', [1], 0, true);// ... du move droite
+    bus.bringToTop();
+
+    // === initialisation les different déplacement du velooo ===
+    velo.bringToTop();
 
     // === initialisation les different déplacement du personnage ===
     player.animations.add('up', [0,1,2,3,4,5,6,7,8], 10, true); // je selection les different image du move avant
@@ -45,6 +65,7 @@ function(){
     this.physics.startSystem(Phaser.Physics.P2JS);
     player.body.fixedRotation = true;
     voiture.body.fixedRotation = true;
+    bus.body.fixedRotation = true;
 	}
   return create;
 });
--- Projet vierge/js/controller/GamePreload.js
+++ Projet vierge/js/controller/GamePreload.js
@@ -6,6 +6,8 @@ function(){
       this.load.image('GreyPixel', 'img/pixelGris.bmp');
       this.load.image('RedPixel', 'img/pixelRouge.bmp');
       this.load.spritesheet('voiture', 'img/voiture.png',240,80,8);
+      this.load.spritesheet('bus', 'img/BusFinal2.png',160,160,4);
+      this.load.spritesheet('velo','img/velo1droit.png',300,300,1);
       // === Variable ===
       var s; // variable du Personnage qui bouge.
       var GreyPixel;// fond gris
@@ -13,7 +15,9 @@ function(){
       var Group;
       var player; // variable du joeur
       var voiture; // la voiture avance !!
+      var bus; // le bus avance !!
       var valeur; // je crée une valeur d'équipement
+      var velo; // velo velo hoooohooohohoo
   	}
   return preload;
 });
