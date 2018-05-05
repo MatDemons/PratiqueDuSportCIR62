a2dc3d0a52c5195ab8f531dad52a46513171e05a
Bastien Verrier <35604545+bastienverrier@users.noreply.github.com>
2018-05-05 18:49:26 +0200

    Generique

--- Projet vierge/js/main.js
+++ Projet vierge/js/main.js
@@ -17,14 +17,14 @@ require.config({
     }
 });
 //=== je charge les different fichier de mon controller ===
-require(['m/fM','c/fC','c/MP','c/MC','c/SP','c/SC','c/SU','c/GP','c/GC','c/GU','c/FP','c/FC','c/FU'],
-function(fM,fC,MP,MC,SP,SC,SU,GP,GC,GU,FP,FC,FU){
+require(['m/fM','c/fC','c/MP','c/MC','c/SP','c/SC','c/SU','c/GP','c/GC','c/GU','c/EC'],
+function(fM,fC,MP,MC,SP,SC,SU,GP,GC,GU,EC){
    var footer = new fM();
    var footerview = new fC(footer, '#footer');
    var game = new Phaser.Game(1000, 450, Phaser.AUTO, 'jeux');
    game.state.add('menu',   { preload: MP ,create: MC  });
    game.state.add('select', { preload: SP ,create: SC,  update: SU  });
    game.state.add('game',   { preload: GP ,create: GC,  update: GU  });
-   game.state.add('finish', { preload: FP ,create: FC,  update: FU  });
+   game.state.add('finish', { create: EC  });
    game.state.start('menu');
 });
